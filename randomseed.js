744e1c0f7673ae7b041a097d3772e0debb30967b
Anastanaze <antoinecani@hotmail.com>
2024-02-04 16:29:17 +0100

    tests marchent randomseed pourrie

---
+++ randomseed.js
@@ -0,0 +1,171 @@
+﻿
+
+var tirage = 0;
+var seedtable;
+
+export function lancerD(d, openroll) {
+  if (openroll == undefined) {
+     openroll = 0;
+   }
+ var inverser =0;  
+ if (!d) {
+   return 0;
+ }
+ else if (d<0) {
+   d=d*-1;
+   inverser = 1;
+ }
+  // alert ("d "+d);
+   var de = Math.random() * d ;
+ de = Math.floor (de) +1;
+   var seuil = -1;
+   if (openroll) {
+       seuil = Math.ceil(d / 20);
+   }
+   if (de > d - seuil) {
+       de = de + lancerD(d, openroll - 1);
+   }
+ if (inverser) {
+   de*=-1;
+ }
+   return de;
+}
+
+export const RandomSeed = function (seed) {
+  if (seed == undefined && seedtable==undefined) {
+  seedtable = new Array (30).fill(-1);
+	var place;
+	//alert ("Place "+place);
+	var valeur = 0;
+    for (var i=0; i<30;++i) {
+		if (i<10) {
+			valeur=i ;
+		}
+		else if (i<20) {
+			valeur = i-10;
+		}
+    else {
+      valeur = i-20;
+    }
+		place = lancerD(10)-1+valeur;
+		while (seedtable[place]>-1) {
+			if (place<29) {
+			  ++place;
+			  }
+			else {
+				place = 0;
+			}
+		}
+			seedtable [place] = valeur ;
+			//alert ("Valeur :"+valeur+"  Place : "+place);
+		}
+	this.seed = seedtable ;	
+  console.log (seedtable);
+	}
+  else {
+   this.seed = seed;
+   } 
+ let tt = "100" + this.seed [this.seed[11]] + this.seed [5]+ this.seed[7] +this.seed[this.seed[14]];
+  this.premierTirage = parseInt (tt);  
+ // alert ("PremierTirage :"+tt);  
+}  
+  	
+  
+RandomSeed.prototype.roll = function (de, tirageNo) {
+   if (tirageNo==undefined) {
+	   tirageNo=tirage; 
+	   ++ tirage ; 
+	   }
+	else {
+       tirageNo = parseInt (tirageNo);
+	}	   
+	
+    tirageNo += this.premierTirage ;
+	
+   let noDe1, noDe2, noDe3, de1, de2, de3, step, result,tiragestring ;
+
+   step = nextS(Math.ceil(tirageNo/119));
+   let t = tirageNo;
+
+   noDe1 = nextS (t);
+   noDe2 = nextS (t+step);
+   noDe3 = nextS (t+2*step);
+   
+   if (tirageNo%3) {
+     de1 = this.seed [noDe2];
+     de2 = this.seed [noDe1];
+   }
+   else {
+     de1 = this.seed [noDe1];
+     de2 = this.seed [noDe2];	   
+   }
+
+   de3 = this.seed [noDe3];
+   
+   if (tirageNo%5) {
+     de3=de1;
+	   de1 = this.seed [noDe3];
+   }
+   else if (tirageNo%7) {
+     de3=de2;
+	   de2 = this.seed [noDe3];
+   }
+
+   if (t%3) {
+    de1=9-de1;
+   }
+   if (t%4) {
+    de2=9-de2;
+   }
+   if (t%5) {
+    de3=9-de3;
+   }
+
+   //console.log ("de1 "+de1);
+   tiragestring = ""+de1+de2+de3;
+   //console.log ("tirage:"+tiragestring);
+   
+    if (!tirageNo%2) {
+	    result = parseInt (tiragestring);
+    }
+    else {
+	    result = 999 - parseInt (tiragestring);
+    }
+ 
+   
+   if (de !=undefined) {
+     result = Math.floor((result*de)/1000) +1;  
+   }
+   else {
+     result = Math.floor((result*de)/10);  
+   }	   
+    //  alert ("Tirage n° :"+tirageNo+" result "+ result);
+   
+   return parseInt(result);
+  
+   function nextS (v) {
+      v=Math.floor(v);
+		   if (v>-1 && v<30) return v;
+       else return v- (Math.floor(v/30)*30);
+   }
+
+   
+}
+
+function testSeed () {
+  s = new RandomSeed () ;
+  var txt = "Randomseed :"+ display (s.seed) + "Premier tirage "+ s.premierTirage ;
+  for (var i=0;i<1000;++i) {
+     txt += + "-";
+  }
+  document.write (txt);
+}  
+
+function convertNumber(number) {
+
+}
+
+	   
+   
+ 
+ 
--- tests.js
+++ tests.js
@@ -1,85 +1,101 @@
+import {RandomSeed} from "./randomseed.js"
+
+var s=new RandomSeed()
+
 const normalRoll1000 = function () {
-    return Math.floor(Math.random()*1000);
+  return Math.floor(Math.random()*1000);
+}
+
+
+const rseedRoll1000 = function () {
+  return s.roll (1000)-1;
 }
 
 const testGeneratedNumbers = function (rollno) {
-    const roll1000=normalRoll1000;
-    let rolls = [];
-    let repeats = new Array (1000).fill(0);
-    for (let i=0;i<rollno;++i) {
-        let roll = roll1000();
-        rolls.push(roll);
-        repeats[roll]=++repeats[roll];
+  const roll1000=rseedRoll1000;
+  let rolls = [];
+  let repeats = new Array (1000).fill(0);
+  for (let i=0;i<rollno;++i) {
+    let roll = roll1000();
+    if (roll==undefined) {
+      console.log ("undefined roll!");
     }
-
-    //test 1 : Nombre max de répétitions (établi en même temps que les tirages)
-    let maxRepeats = 0;
-    let minRepeats = 1000000;
-    let maxRepeated = -1;
-    let minRepeated = -1;
-    for (let i=0;i<rolls.length;++i) {
-        if (repeats[rolls[i]]>maxRepeats) {
-            maxRepeats = repeats[rolls[i]];
-            maxRepeated = rolls[i];
-        }
+    rolls.push(roll);
+    repeats[roll]=++repeats[roll];
+  }
+  
+  //test 1 et 2 : Nombre max et min de répétitions d'un nombre 
+  let maxRepeats = 0;
+  let minRepeats = 1000000;
+  let maxRepeated = -1;
+  let minRepeated = -1;
+  for (let i=0;i<1000;++i) {
+    if (repeats[i]>maxRepeats) {
+      maxRepeats = repeats[i];
+      maxRepeated = i;
     }
-    console.log ("Test n°1 : Le nombre le plus répété l'a été "+maxRepeats+" fois. C'était "+maxRepeated+".");
+    if (repeats[i]<minRepeats) {
+      minRepeats = repeats[i];
+      minRepeated = i;
+    }
+  }
+  console.log ("Test n°1 : Le nombre le plus répété l'a été "+maxRepeats+" fois. C'était "+maxRepeated+".");
+  console.log ("Test n°2 : Le nombre le moins répété l'a été "+minRepeats+" fois. C'était "+minRepeated+".");
+  
+  //test 3 : Répétition de séries
+  console.log ("Test n°3 : Nombre de répétitions de séries de nombres (ça peut être long)...");
+  let numberOfSeriesFound = [0,0,0,0,0,0,0,0,0,0];
+  let seriesFound = [];
+  let reducedRolls, startIndex, serieToTest, seriesToTest, ignoreSerie;
+  
+  
+  
+  for (let i=0;i<1000;++i) { //Teste les séries commençant par i
+    reducedRolls= rolls;
+    seriesToTest = [];
     
-    //test 2 : Nombre min de répétitions
-    for (let i=0; i<1000; ++i) {
-        if (repeats[i]<minRepeats) {
-            minRepeats = repeats[i];
-            minRepeated = i;
-        }
+    while (reducedRolls.indexOf(i)!=-1) {  // liste les séries commençant par i
+      startIndex = reducedRolls.indexOf(i);
+      serieToTest=[];
+      for (let a=0;a<10;a++) {
+        serieToTest.push (reducedRolls[startIndex+a]);
+      }
+      seriesToTest.push (serieToTest);
+      reducedRolls=reducedRolls.slice (startIndex+1);
     }
-    console.log ("Test n°2 : Le nombre le moins répété l'a été "+minRepeats+" fois. C'était "+minRepeated+".");
-
-    //test 3 : Répétition de séries
-    console.log ("Test n°3 : Nombre de répétitions de séries de nombres (ça peut être long)...")
-
-    let reducedRolls, startIndex, serieToTest, seriesToTest, reducedSeriesToTest, elementNumber, serieSize ;
-    let numberOfSeriesFound = [0,0,0,0,0,0,0,0,0,0];
-
-    for (let i=0;i<1000;++i) {
-        reducedRolls= rolls;
-        seriesToTest = [];
-        //console.log ("Teste les séries commençant par "+i);
-
-        while (reducedRolls.indexOf(i)!=-1) {
-            startIndex = reducedRolls.indexOf(i);
-            serieToTest=[];
-            for (let a=1;a<10;a++) {
-                serieToTest.push (reducedRolls[startIndex+a]);
+    ignoreSerie=new Array(seriesToTest.length).fill(false);
+    
+    for (let j=0; j<seriesToTest.length;++j) {
+      if (!ignoreSerie[j]) {
+        for (let k=j+1; k<seriesToTest.length;++k) {
+          for (let l=1;l<10;l++) {
+            if (seriesToTest[j][l]!=seriesToTest[k][l]) {
+              break; }
+              
+              if (l>1) {
+                const serieFound = seriesToTest[j].slice(0,l);
+                seriesFound.push (serieFound);
+                numberOfSeriesFound[l-2]++;
+                ignoreSerie[k]=true;
+                //console.log ("Trouvé répétition de "+(l)+" nombres.");
+              }
             }
-            seriesToTest.push (serieToTest);
-            reducedRolls=reducedRolls.slice (startIndex+1);
+          }
         }
-
-        for (let j=0; j<seriesToTest.length;++j) {
-                reducedSeriesToTest=seriesToTest.slice(j+1);
-                //console.log ('Cherche série '+seriesToTest[j]);
-                for (let k=0; k<reducedSeriesToTest.length;++k) {
-                  //  console.log ("Compare avec "+reducedSeriesToTest[k]);
-                    for (let l=0;l<10;l++) {
-                        if (seriesToTest[j][l]!=reducedSeriesToTest[k][l]) {
-                            break; }
-                        else {
-                         numberOfSeriesFound[l]++;
-                         //console.log ("Trouvé répétition de "+(l+2)+" nombres.");
-                        }
-                    }
-
-                }
-            }
-        }
-        for (let i=0;i<10;++i) {
-            if (numberOfSeriesFound[i]==0) {break;}
-            console.log ("Au total "+numberOfSeriesFound[i]+" répétitions de séries de "+(i+2)+" nombres trouvées.");
-        }
+      }    
     }
-
-
-
-
-
-testGeneratedNumbers(100000)
+    for (let i=0;i<10;++i) {
+      //       //  if (numberOfSeriesFound[i]==0) {break;}
+      console.log ("Au total "+numberOfSeriesFound[i]+" répétitions de séries de "+(i+2)+" nombres trouvées.");
+    }
+    // for (let i=0;i<seriesFound.length;++i) {
+    //   console.log ("Série trouvée :"+seriesFound[i]);
+    // }
+    console.log (rolls);
+  }
+  
+  
+  
+  
+  
+  testGeneratedNumbers(100000)
